690ea7fd7ec685a7949cec5b591fafa5fc4bb5f0
shenyuanmengyi <316146650@qq.com>
2014-09-04 14:51:19 +0800

    修改漏洞

--- zxics/zxics/serviceDirectory/serviceIndex/serviceIndex.m
+++ zxics/zxics/serviceDirectory/serviceIndex/serviceIndex.m
@@ -49,8 +49,15 @@
 //个人管理页面跳转
 -(IBAction)personindex:(id)sender
 {
-    personIndex *_personIndex=[[personIndex alloc]init];
-    [self.navigationController pushViewController:_personIndex animated:NO];
+    AppDelegate *myDelegate = [[UIApplication sharedApplication] delegate];
+    if (myDelegate.entityl) {
+        personIndex *_personIndex=[[personIndex alloc]init];
+        [self.navigationController pushViewController:_personIndex animated:NO];
+    }else{
+        NSString *rowString =@"请先登陆！";
+        UIAlertView * alter = [[UIAlertView alloc] initWithTitle:@"提示" message:rowString delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [alter show];
+    }
 }
 
 //下载管理页面跳转
